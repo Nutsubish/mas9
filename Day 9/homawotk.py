eb0ff86a0f954887f3e998405c42fda84877355a
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-22 15:10:40 +0400

    done

---
+++ Day 9/homawotk.py
@@ -0,0 +1,10 @@
+#shemovataninot tavisi asaki da how much asakic aqvs gavaketot pwinti magasi imdenje da 12 wlisas daweve goashi shemoxvedi(es chemze is gaketebuli 12 weli mainc dont diffs 8 wlisas to 12 wlisas)
+
+# es gaketebuli maqvs gakv 10 shi kentzec da luwebzec naxet 
+
+
+age=int(input("yo age:"))
+while age >0:
+    print("gilocav",age)
+    age=age-1
+   
